fd139341446deab2f49cc42e1b4587083d323d2d
Anubhav Jain <jainanubhav2003@gmail.com>
2026-03-08 23:10:30 +0530

    Addition of ValidationChecks

--- student-management-impl/src/main/java/org/anubhav/student_management/utility/ValidationChecks.java
+++ student-management-impl/src/main/java/org/anubhav/student_management/utility/ValidationChecks.java
@@ -1,13 +1,25 @@
 package org.anubhav.student_management.utility;
 
 import lombok.experimental.UtilityClass;
+import org.anubhav.student_management.exception.validation.IllegalPathParameterException;
 
 @UtilityClass
 public class ValidationChecks {
-    public void validateParameter(String parameter, String parameterName, int expectedLength) {
-        if (parameter.isBlank() || parameter.length() != expectedLength) {
-            throw new IllegalArgumentException(
-                    String.format("Parameter %s cannot be null or less than %d.", parameterName, expectedLength)
+
+    public void validateParameter(String parameter, String parameterName, int exLength) {
+        if (parameter.isBlank() || parameter.length() != exLength) {
+            throw new IllegalPathParameterException(
+                    String.format("%s cannot be null and length cannot be smaller than %d.", parameterName, exLength),
+                    parameterName
+            );
+        }
+    }
+
+    public void validateParameterValue(String parameter, String parameterName) {
+        if (!parameter.matches("-?\\d+")) {
+            throw new IllegalPathParameterException(
+                    String.format("%s should be a number.", parameterName),
+                    parameterName
             );
         }
     }
